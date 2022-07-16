5b13743729d5ec197926cb0429d521f45e484503
tanisq23 <tanisq.m@gmail.com>
2022-07-16 17:35:51 +0530

    First commit

---
+++ Day 3/fibonacci.js
@@ -0,0 +1,10 @@
+function printfFibonacci10() {
+    var fibonacci10 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    for (var i = 0;  i < 10; i ++) {
+        console.log(fibonacci10[i]);
+    }
+}
+
+printfFibonacci10()
+
+
---
+++ Day 4/main.js
@@ -0,0 +1,42 @@
+const movies = [
+    {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+    {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+    {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+    {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+    {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+    {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+    {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+    {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+    {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+    {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+    {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+   ]
+   
+
+
+const guessInput = document.getElementById('guess-input')
+
+const movieIndex = Math.floor(Math.random() * movies.length);
+
+explanation.innerHTML = movies[movieIndex].explanation;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (guessInput.value == movies[movieIndex].title) {
+        result.innerHTML = "You guessed correctly"
+        result.classList.remove('alert-danger');
+        result.classList.add('alert-success');
+    } else{
+        result.innerHTML = "You guessed incorrectly, the movie was " + movies[movieIndex].title;
+        result.classList.remove('alert-success');
+        result.classList.add('alert-danger');
+    }
+});
+
+
+form.addEventListener('button', (event) => {
+    event.preventDefault();
+    hint.InnerHTML = movies[movieIndex].hint;
+});
+
